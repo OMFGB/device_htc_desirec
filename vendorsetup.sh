c56ab2c0e31663b705a1cdd4db1758876dc7d199
Ken <r2doesinc@poiteeserver.com>
2011-03-27 19:00:11 -0400

    remove unused lunch combos

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,3 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_desirec-userdebug
-add_lunch_combo full_desirec-eng
